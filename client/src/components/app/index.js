0a48f926f96eec0e3e4a441084a67d606c2d4cd4
lai-tran-the-vinh <thevinh.development@gmail.com>
2025-07-18 15:54:08 +0700

    Feature: Create default avatar component

--- client/src/components/app/index.js
+++ client/src/components/app/index.js
@@ -4,3 +4,4 @@ export { default as ErrorToast } from './ErrorToast';
 export { default as ImagesSlider } from './ImagesSlider';
 export { default as SuccessToast } from './SuccessToast';
 export { default as LoadingToast } from './LoadingToast';
+export { default as AvatarDefault } from './AvatarDefault';
